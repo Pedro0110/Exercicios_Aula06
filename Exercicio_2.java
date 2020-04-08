c11fb9ad9355f03d816d96c5b1c428230dbcecbf
Pedro0110 <63135543+Pedro0110@users.noreply.github.com>
2020-04-08 01:18:26 -0300

    Envio do exercicio

---
+++ Exercicio_2.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+    int resultadoProgrecao = 0;
+    
+    for(int i = 1; i <= 30; i++){
+       resultadoProgrecao += i; 
+    }
+    
+    System.out.println("Progressão aritmética de 1 a 30 é : " + resultadoProgrecao);
+    }
+}
